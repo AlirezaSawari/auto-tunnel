17e2ef740e9cb1157018c20e7c3614b1c2cc5d35
Alireza Sawari <alisawari@yahoo.com>
2022-11-14 01:33:15 +0330

    Solve some typing problems

---
+++ README.md
@@ -0,0 +1,13 @@
+# traffic balancer and auto tunneler
+A sample script to create fake upload traffic and tunnel automatically
+
+## Installation
+
+You need two servers, the first server is the server where you want to increase upload traffic. And the second server is the server that is going to download from the first server
+In this tutorial, we call the first server foreignVPS and the second server domesticVPS
+
+
+```bash
+curl -o install.sh -L https://raw.githubusercontent.com/AlirezaSawari/auto-tunnel/main/install.sh && bash install.sh
+```
+    
--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 
 distro=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
 fileLink='http://152.89.46.241/dl'
-myIP=$(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
+thisServerIP=$(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
 networkInterfaceName=$(ip -o -4 route show to default | awk '{print $5}')
 
 if [[ $distro != "ubuntu" ]]; then
@@ -17,8 +17,8 @@ if [[ $distro != "ubuntu" ]]; then
 fi
 
 echo "This is foreignVPS or domesticVPS?"
-echo "   1) foreignVPS"
-echo "   2) domesticVPS"
+echo "   1) domestic server"
+echo "   2) foreign server"
 read -r -p "Please select one [1-2]: " -e OPTION
 case $OPTION in
 1)
@@ -26,18 +26,18 @@ case $OPTION in
   iptables -I INPUT -p tcp -m tcp --dport 443 -j ACCEPT
   iptables -I INPUT -p udp -m udp --dport 80 -j ACCEPT
   iptables -I INPUT -p udp -m udp --dport 443 -j ACCEPT
-  read -r -p "do you want tunnel to domesticVPS?(y/n) " -e TUNNEL_OPTION
+  read -r -p "do you want tunnel to foreign server?(y/n) " -e TUNNEL_OPTION
   if [[ "$TUNNEL_OPTION" == "y" || "$TUNNEL_OPTION" == "Y"  || "$TUNNEL_OPTION" == "yes" ]]; then
     echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
     sysctl -p
-    iptables -t nat -I PREROUTING -p tcp --dport 80 -j DNAT --to-destination "$myIP"
-    iptables -t nat -I PREROUTING -p udp --dport 80 -j DNAT --to-destination "$myIP"
-    iptables -t nat -I PREROUTING -p tcp --dport 443 -j DNAT --to-destination "$myIP"
-    iptables -t nat -I PREROUTING -p udp --dport 443 -j DNAT --to-destination "$myIP"
-    iptables -t nat -I PREROUTING -p tcp --dport 22 -j DNAT --to-destination "$myIP"
-    echo "Enter domesticVPS IP:"
-    read -r domesticVPSIP
-    iptables -t nat -A PREROUTING -j DNAT --to-destination "$domesticVPSIP"
+    iptables -t nat -I PREROUTING -p tcp --dport 80 -j DNAT --to-destination "$thisServerIP"
+    iptables -t nat -I PREROUTING -p udp --dport 80 -j DNAT --to-destination "$thisServerIP"
+    iptables -t nat -I PREROUTING -p tcp --dport 443 -j DNAT --to-destination "$thisServerIP"
+    iptables -t nat -I PREROUTING -p udp --dport 443 -j DNAT --to-destination "$thisServerIP"
+    iptables -t nat -I PREROUTING -p tcp --dport 22 -j DNAT --to-destination "$thisServerIP"
+    echo "Enter foreign server IP:"
+    read -r foreignVPSIP
+    iptables -t nat -A PREROUTING -j DNAT --to-destination "$foreignVPSIP"
     iptables -t nat -A POSTROUTING -j MASQUERADE -o "$networkInterfaceName"
     echo "tunnel is done"
   fi
@@ -45,9 +45,9 @@ case $OPTION in
   wget $fileLink -O /var/www/html/dl
   iptables-save > /etc/iptables/rules.v4
   ip6tables-save > /etc/iptables/rules.v6
-  echo "your server is ready and this is a file for download by domesticVPS:"
-  echo "http://$myIP/dl"
-  echo "copy this link and use in domesticVPS"
+  echo "your server is ready and this is a file for download by foreign server:"
+  echo "http://$thisServerIP/dl"
+  echo "copy this link and use in foreign server"
   ;;
 2)
     apt update -y && apt upgrade -y && apt install wget cron -y
